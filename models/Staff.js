ebbc6fbd3654e94f3ad3b5da8be0edac25f8fb70
YraKot <y.kitsera@gmail.com>
2017-06-26 12:49:48 +0300

    add biography email

--- models/Staff.js
+++ models/Staff.js
@@ -12,12 +12,15 @@ var Staff = new keystone.List('Staff', {
 Staff.add({
     title: { type: String, required: true },
     image: { type: Types.CloudinaryImage },
+    email: { type: Types.Html, wysiwyg: true, height: 20 },
     degree: { type: Types.Html, wysiwyg: true, height: 50 },
     position: {type: Types.Html, wysiwyg: true, height: 50 },
     subjects: {type: Types.Html, wysiwyg: true, height: 100 },
     interests: {type: Types.Html, wysiwyg: true, height: 100 },
     scientific: {type: Types.Html, wysiwyg: true, height: 400 },
-    methodological: {type: Types.Html, wysiwyg: true, height: 400 }
+    methodological: {type: Types.Html, wysiwyg: true, height: 400 },
+    biography: { type: Types.Html, wysiwyg: true, height: 400 },
+    internship: { type: Types.Html, wysiwyg: true, height: 200 }
 });
 
 Staff.register();
