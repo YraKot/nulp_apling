15c66fce1a9e4585a56550d4b9e35f66c3ad0364
YraKot <y.kitsera@gmail.com>
2017-06-27 13:02:56 +0300

    add modal

--- models/Teaching.js
+++ models/Teaching.js
@@ -1,6 +1,16 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
-var Teaching = new keystone.List('Teaching');
+var Teaching = new keystone.List('Teaching', {
+     map: { name: 'title' },
+     autokey: { path: 'slug', from: 'title', unique: true}
+});
+
+Teaching.add({
+    title: { type: String, required: true },
+    bachelor: { type: Types.Html, wysiwyg: true, height: 200 },
+    master: { type: Types.Html, wysiwyg: true, height: 200 },
+    prof: { type: Types.Html, wysiwyg: true, height: 200 }
+});
 
 Teaching.register();
